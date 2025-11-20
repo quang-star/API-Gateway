872b8ce0d759741e4841d25a7944d3982cfb732e
unknown <nguyenkimquang1612@gmail.com>
2025-11-20 21:11:19 +0700

    update config/env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,10 +2,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// AUTH_SERVICE_URL=http://127.0.0.1:8001
+// CHAT_SERVICE_URL=http://127.0.0.1:8002
+// ADMIN_SERVICE_URL=http://127.0.0.1:8003
+// POST_SERVICE_URL=http://127.0.0.1:8004
+
 export const config = {
   port: Number(process.env.PORT) || 4000,
   frontendOrigin: process.env.FRONTEND_ORIGIN || 'http://localhost:5173',
 
-  userServiceUrl: process.env.USER_SERVICE_URL || 'http://127.0.0.1:8001',
-  orderServiceUrl: process.env.ORDER_SERVICE_URL || 'http://127.0.0.1:8002',
+  authServiceUrl: process.env.AUTH_SERVICE_URL || 'http://127.0.0.1:8001',
+  chatServiceUrl: process.env.CHAT_SERVICE_URL || 'http://127.0.0.1:8002',
+  adminServiceUrl: process.env.ADMIN_SERVICE_URL || 'http://127.0.0.1:8003',
+  postServiceUrl: process.env.POST_SERVICE_URL || 'http://127.0.0.1:8004',
 };
